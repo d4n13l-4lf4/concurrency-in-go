7b0ceab7ef62d4a673a3e25b1bf29dad95fbb939
d4n13l-4lf4 <d.aucatoma96@gmail.com>
2020-09-07 17:20:21 -0500

    Go routine variable reference

--- chapter03/goroutine_variable_reference.go
+++ chapter03/goroutine_variable_reference.go
@@ -10,6 +10,7 @@ func main() {
 	var wg sync.WaitGroup
 	for _, salutation := range []string{"hello", "greetings", "good day"} {
 		wg.Add(1)
+		/* Should pass salutation var to prevent taking the last value "good day" */
 		go func(salutation string) {
 			defer wg.Done()
 			fmt.Println(salutation)
