5ba33cad10cfc21887d46e87712389840aba26ed
d4n13l-4lf4 <d.aucatoma96@gmail.com>
2020-09-08 12:32:57 -0500

    the select statement

---
+++ chapter03/first_select.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+
+	start := time.Now()
+	c := make(chan interface{})
+	go func() {
+		time.Sleep(5 * time.Second)
+		close(c)
+	}()
+
+	fmt.Println("Blocking on read...")
+	select {
+	case <-c:
+		fmt.Printf("Unblocked %v later.\n", time.Since(start))
+	/* A timeout */
+	case <- time.After(1 * time.Second):
+		fmt.Println("Timed out.")
+	default:
+		fmt.Printf("In default after %v\n\n", time.Since(start))
+	}
+}
